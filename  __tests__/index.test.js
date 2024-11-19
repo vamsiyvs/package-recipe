84be04009e5e47cf147fb9305202ef97121af564
Vamsi Krishna Jannu <v.jannu@wealthpilot.de>
2024-11-19 15:26:43 +0100

    first

---
+++  __tests__/index.test.js
@@ -0,0 +1,6 @@
+describe('index.js', () => {
+    it('greet function returns Hello world!', () => {
+      const greet = require('../src/index')
+      expect(greet()).toBe('Hello world!')
+    })
+  })
